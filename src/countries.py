ac8f16dfae6bd194cad2abe4bb265f26ef476110
Óscar Delgado <odelgado@citibeats.com>
2023-03-24 10:45:03 +0100

    Documentation & better naming

--- src/countries.py
+++ src/countries.py
@@ -5,17 +5,47 @@ from src.languages import get_supported_languages
 
 
 def get_available_countries() -> List[str]:
+    """
+    Get available countries.
+
+    Returns:
+        List[str]: List of countries in ISO 3166 alpha-2 format
+    """
     languages = get_supported_languages()
     return [k for k, v in COUNTRIES.items() if any(lang in languages for lang in v["languages"])]
 
 
 def get_countries() -> List[str]:
+    """
+    Get all countries.
+
+    Returns:
+        List[str]: List of countries in ISO 3166 alpha-2 format
+    """
     return COUNTRIES.keys()
 
 
-def get_country(alpha_2: str) -> Dict:
-    return COUNTRIES.get(alpha_2, {})
+def get_country(country_code: str) -> Dict:
+    """
+    Get information of a country.
+
+    Args:
+        country_code (str): Country code in ISO 3166 alpha-2 format
+
+    Returns:
+        Dict: Country's information
+    """
+    return COUNTRIES.get(country_code, {})
 
 
-def get_languages(alpha_2: str) -> List[str]:
-    return COUNTRIES.get(alpha_2, {})["languages"]
+def get_languages(country_code: str) -> List[str]:
+    """
+    Get all languages from a country.
+
+    Args:
+        country_code (str): Country code in ISO 3166 alpha-2 format
+
+    Returns:
+        List[str]: List of languages in ISO 639-1 alpha-2 format
+    """
+    return COUNTRIES.get(country_code, {})["languages"]
--- src/languages.py
+++ src/languages.py
@@ -4,12 +4,33 @@ from src.constants import SUPPORTED_LANGUAGES
 
 
 def get_supported_languages() -> List[str]:
+    """
+    Get supported languages.
+
+    Returns:
+        List[str]: List of languages in ISO 639-1 alpha-2 format
+    """
     return [k for k, v in SUPPORTED_LANGUAGES.items() if all(v["webhooks"].values())]
 
 
 def get_languages() -> List[str]:
+    """
+    Get all languages.
+
+    Returns:
+        List[str]: List of languages in ISO 639-1 alpha-2 format
+    """
     return SUPPORTED_LANGUAGES.keys()
 
 
-def get_language(alpha_2: str) -> Dict:
-    return SUPPORTED_LANGUAGES.get(alpha_2, {})
+def get_language(language_code: str) -> Dict:
+    """
+    Get language's information.
+
+    Args:
+        language_code (str): Language code in ISO 639-1 alpha-2 format
+
+    Returns:
+        Dict: Language's information
+    """
+    return SUPPORTED_LANGUAGES.get(language_code, {})
