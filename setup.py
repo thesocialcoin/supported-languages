71e76bd97676eba2742280e60e5875f980e84a32
Óscar Delgado <odelgado@citibeats.com>
2023-03-27 11:37:03 +0200

    🔩 Added setup.py file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from src import __version__
+
+setup(
+    name='supported-languages',
+    version=__version__,
+
+    url='https://github.com/thesocialcoin/supported-languages',
+    author='Oscar Delgado',
+    author_email='odelgado@citibeats.com',
+
+    py_modules=['supported-languages'],
+)
