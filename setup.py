be49e57a9ccf2dfdd98d4caa748cd1563cfdafb8
Óscar Delgado <odelgado@citibeats.com>
2023-03-27 13:12:28 +0200

    Changed configuration

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 from src import __version__
 
@@ -10,5 +10,5 @@ setup(
     author='Oscar Delgado',
     author_email='odelgado@citibeats.com',
 
-    py_modules=['src'],
+    py_modules=find_packages()
 )
