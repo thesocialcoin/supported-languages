bf3d8008156c64063a247d853b95d8950723a149
annaruizferrer <annaruizferrer@gmail.com>
2023-05-05 14:04:35 +0200

    fix fs

--- supported_languages/constants.js
+++ supported_languages/constants.js
@@ -1,5 +1,9 @@
-var fs = require('fs');
+import fs from 'fs';
 
 export const SUPPORTED_LANGUAGES = JSON.parse(fs.readFileSync('./supported_languages/supported_languages.json', 'utf8'));
 
+
 export const COUNTRIES = JSON.parse(fs.readFileSync('./supported_languages/countries.json', 'utf8'));
+
+console.log(SUPPORTED_LANGUAGES)
+console.log(COUNTRIES)
